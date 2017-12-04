e7290e2a6e01649663edd76707835826105f1276
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2017-12-04 19:13:10 +0530

    fix documentation to it builds correctly

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -366,4 +366,4 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://beamparticle.org/': None}
+# intersphinx_mapping = {'http://beamparticle.org/': None}
