fc8b93aa277597e58082e4758633c7408f7ce7e4
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2017-12-05 21:43:40 +0530

    First time install need not be painful
    Ensure that the first time install has user and
    password from sys.config (root/root). Change if
    for your deployment if you like.
    Subsequently, the user create user in each of the
    categories by running command on websocket window
    as follows:
    
        run beamparticle_auth:create_user(<<"user">>, <<"pwd">>, websocket)
    
    And http_rest user as follows:
    
        run beamparticle_auth:create_user(<<"user">>, <<"pwd">>, http_rest)
    
    Once at least one user in that category is created, then the default
    password in sys.config cannot be used.

--- src/beamparticle_auth.erl
+++ src/beamparticle_auth.erl
@@ -27,42 +27,79 @@
 
 -export([authenticate_user/2, create_user/3]).
 
+-include("beamparticle_constants.hrl").
+
+
+%% @doc Get prefix used to store user auth
+%% @private
+-spec get_user_storage_prefix(websocket | http_rest) -> binary().
+get_user_storage_prefix(websocket) ->
+    <<"websocket-">>;
+get_user_storage_prefix(http_rest) ->
+    <<"http_rest-">>.
+
+%% @doc Are there any users in datastore for auth
+-spec any_user_exists(Prefix :: binary() | websocket | http_rest) -> boolean().
+any_user_exists(websocket = Type) ->
+    Prefix = get_user_storage_prefix(Type),
+    any_user_exists(Prefix);
+any_user_exists(http_rest = Type) ->
+    Prefix = get_user_storage_prefix(Type),
+    any_user_exists(Prefix);
+any_user_exists(Prefix) when is_binary(Prefix) ->
+    PrefixLen = byte_size(Prefix),
+    Fn = fun({K, _V}, AccIn) ->
+                 {R, S2} = AccIn,
+                 case beamparticle_storage_util:extract_key(K, user) of
+                     undefined ->
+                         throw({{ok, R}, S2});
+                     <<Prefix:PrefixLen/binary, _/binary>> = ExtractedKey ->
+                         %% return as soon as we get one key which matches
+                         throw({{ok, [ExtractedKey | R]}, S2});
+                     _ ->
+                         throw({{ok, R}, S2})
+                 end
+         end,
+    {ok, Resp} = beamparticle_storage_util:lapply(Fn, Prefix, user),
+	Resp =/= [].
 
 %% TODO FIXME - keep user/password outside of beamparticle_storage_util for security
-create_user(User, Password, websocket) ->
+create_user(User, Password, Type) when Type == websocket orelse Type == http_rest ->
     HashedPassword = crypto:hash(sha, Password),
-    beamparticle_storage_util:write(<<"websocket-", User/binary>>, HashedPassword, user);
-create_user(User, Password, http_rest) ->
-    HashedPassword = crypto:hash(sha, Password),
-    beamparticle_storage_util:write(<<"http_rest-", User/binary>>, HashedPassword, user).
+    Prefix = get_user_storage_prefix(Type),
+    beamparticle_storage_util:write(
+      <<Prefix/binary, User/binary>>, HashedPassword, user).
 
 %% @todo separate authentication based on medium
 %% (websocket or http_rest) and also on request path.
 -spec authenticate_user(cowboy_req:req(), websocket | http_rest) ->
     {true | false, cowboy_req:req(), undefined | term()}.
-authenticate_user(Req, websocket) ->
+authenticate_user(Req, Type) when Type == websocket orelse Type == http_rest ->
     case cowboy_req:parse_header(<<"authorization">>, Req) of
 		{basic, User, Password} ->
+            Prefix = get_user_storage_prefix(Type),
             HashedPassword = crypto:hash(sha, Password),
-            case beamparticle_storage_util:read(<<"websocket-", User/binary>>, user) of
-                {ok, HashedPassword} ->
-                    {true, Req, User};
-                _ ->
-                    {false, Req, undefined}
-            end;
-		_ ->
-			{false, Req, undefined}
-	end;
-authenticate_user(Req, http_rest) ->
-    case cowboy_req:parse_header(<<"authorization">>, Req) of
-		{basic, User, Password} ->
-            HashedPassword = crypto:hash(sha, Password),
-            case beamparticle_storage_util:read(<<"http_rest-", User/binary>>, user) of
-                {ok, HashedPassword} ->
-                    {true, Req, User};
-                _ ->
-                    {false, Req, undefined}
+            case any_user_exists(Type) of
+                true ->
+                    case beamparticle_storage_util:read(<<Prefix/binary, User/binary>>, user) of
+                        {ok, HashedPassword} ->
+                            {true, Req, User};
+                        _ ->
+                            {false, Req, undefined}
+                    end;
+                false ->
+                    {ok, AuthConfig} = application:get_env(?APPLICATION_NAME, auth),
+                    DefaultUser = proplists:get_value(default_user, AuthConfig),
+                    DefaultPassword = proplists:get_value(default_password, AuthConfig),
+                    DefaultHashedPassword = crypto:hash(sha, DefaultPassword),
+                    case {User, HashedPassword} of
+                        {DefaultUser, DefaultHashedPassword} ->
+                            {true, Req, User};
+                        _ ->
+                            {false, Req, undefined}
+                    end
             end;
 		_ ->
 			{false, Req, undefined}
 	end.
+
