c417d30861865dd3aa06efbc69adcfeb4d27dcaf
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2017-12-10 19:09:10 +0530

    discover function calls within a given function
    This will pave the way for indexing function usage,
    thereby giving critical information related to function
    usage within each other. This is critical to get an
    understanding of which functions are used wherein
    so an impact analysis can be made when the
    function changes.

--- src/beamparticle_erlparser.erl
+++ src/beamparticle_erlparser.erl
@@ -22,7 +22,8 @@
 
 -export([
     evaluate_erlang_expression/1,
-    calltrace_to_json_map/1
+    calltrace_to_json_map/1,
+    discover_function_calls/1
 ]).
  
 %% @doc Evaluate a given Erlang expression and give back result.
@@ -99,3 +100,84 @@ intercept_local_function(FunctionName, Arguments) ->
             end
     end.
  
+%% @doc Discover function calls from the given anonymous function.
+%%
+%% This function recurisively digs into the function body and finds
+%% any further function calls (local or remote) and returns back
+%% unique list of function calls made by the provided anonymous
+%% function. You could either give the function definition or
+%% compiled function.
+%%
+%% ```
+%%   {ok, Content} = file:read_file("sample-2.erl.fun"),
+%%   beamparticle_erlparser:discover_function_calls(Content).
+%% '''
+-spec discover_function_calls(fun() | string() | binary()) -> any().
+discover_function_calls(ErlangExpression) when is_binary(ErlangExpression) ->
+    discover_function_calls(binary_to_list(ErlangExpression));
+discover_function_calls(ErlangExpression) when is_list(ErlangExpression) ->
+    {ok, ErlangTokens, _} = erl_scan:string(ErlangExpression),
+    {ok, ErlangParsedExpressions} = erl_parse:parse_exprs(ErlangTokens),
+    Functions = recursive_dig_function_calls(ErlangParsedExpressions, []),
+    lists:usort(Functions);
+discover_function_calls(F) when is_function(F) ->
+    {env, FunCodeAsEnv} = erlang:fun_info(F, env),
+    Functions = recursive_dig_function_calls(FunCodeAsEnv, []),
+    lists:usort(Functions).
+
+%% @private
+%% @doc Recursively dig and extract function calls (if any)
+%%
+%% The function returns a list of {Module, Fun, Arity} or
+%% {Fun, Arity} (for local functions).
+-spec recursive_dig_function_calls(term(), list())
+    -> [{atom(), atom(), integer()} | {atom(), integer()}].
+recursive_dig_function_calls([], AccIn) ->
+    AccIn;
+recursive_dig_function_calls([H | Rest], AccIn) ->
+    AccIn2 = recursive_dig_function_calls(H, AccIn),
+    recursive_dig_function_calls(Rest, AccIn2);
+recursive_dig_function_calls({[], _, _, Clauses}, AccIn) ->
+    %% This matches the case when erlang:fun_info/1 is used
+    recursive_dig_function_calls(Clauses, AccIn);
+recursive_dig_function_calls({'fun', _LineNum, ClausesRec}, AccIn) ->
+    recursive_dig_function_calls(ClausesRec, AccIn);
+recursive_dig_function_calls({clauses, [H | RestClauses]}, AccIn) ->
+    AccIn2 = recursive_dig_function_calls(H, AccIn),
+    recursive_dig_function_calls(RestClauses, AccIn2);
+recursive_dig_function_calls({clause, _LineNum, _, _, Expressions}, AccIn) ->
+    recursive_dig_function_calls(Expressions, AccIn);
+recursive_dig_function_calls({match, _LineNum, Lhs, Rhs}, AccIn) ->
+    AccIn2 = recursive_dig_function_calls(Lhs, AccIn),
+    recursive_dig_function_calls(Rhs, AccIn2);
+recursive_dig_function_calls({var, _LineNum, _VarName}, AccIn) ->
+    AccIn;
+recursive_dig_function_calls({op, _LineNum, _Op, Lhs, Rhs}, AccIn) ->
+    %% binary operator
+    AccIn2 = recursive_dig_function_calls(Lhs, AccIn),
+    recursive_dig_function_calls(Rhs, AccIn2);
+recursive_dig_function_calls({op, _LineNum, _Op, Rhs}, AccIn) ->
+    %% unary operator
+    recursive_dig_function_calls(Rhs, AccIn);
+recursive_dig_function_calls({'case', _LineNum, Condition, Branches}, AccIn) ->
+    AccIn2 = recursive_dig_function_calls(Condition, AccIn),
+    recursive_dig_function_calls(Branches, AccIn2);
+recursive_dig_function_calls({call, _LineNum, {atom, _LineNum2, FunNameAtom}, Args}, AccIn) ->
+    Arity = length(Args),
+    recursive_dig_function_calls(Args, [{FunNameAtom, Arity} | AccIn]);
+recursive_dig_function_calls({call, _LineNum, {remote, _LineNum2, {atom, _LineNum3, ModuleNameAtom}, {atom, _LineNum4, FunNameAtom}}, Args}, AccIn) ->
+    Arity = length(Args),
+    recursive_dig_function_calls(Args, [{ModuleNameAtom, FunNameAtom, Arity} | AccIn]);
+recursive_dig_function_calls({map, _LineNum, _Var, Fields}, AccIn) ->
+    recursive_dig_function_calls(Fields, AccIn);
+recursive_dig_function_calls({map_field_assoc, _LineNum, Key, Value}, AccIn) ->
+    AccIn2 = recursive_dig_function_calls(Key, AccIn),
+    recursive_dig_function_calls(Value, AccIn2);
+recursive_dig_function_calls({tuple, _LineNum, Fields}, AccIn) ->
+    recursive_dig_function_calls(Fields, AccIn);
+recursive_dig_function_calls({cons, _LineNum, Arg1, Arg2}, AccIn) ->
+    AccIn2 = recursive_dig_function_calls(Arg1, AccIn),
+    recursive_dig_function_calls(Arg2, AccIn2);
+recursive_dig_function_calls(_, AccIn) ->
+    AccIn.
+
