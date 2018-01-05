4680277939b469aac75405e4abb8b323cefcd1fb
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2018-01-05 20:08:08 +0530

    Emulate gen_server:call/2 in java
    Although the java-beamparticle node runs automatically,
    but the following run starts another javanode
    so that the logs are clearly visible.
    
        # ... run the release
        (beamparticle@127.0.0.1)1> gen_server:call({javaserver,
        'java@127.0.0.1'}, {'JavaClassName', 'JavaFunctionName', []}).
        {error,"java.lang.ClassNotFoundException: JavaClassName"}
        (beamparticle@127.0.0.1)2>
        (beamparticle@127.0.0.1)3> net_adm:names().
        {ok,[{"java-beamparticle",33997},{"beamparticle",9002}]}
    
        $ cd priv/javanode/bin
        $ ./javanode "java@127.0.0.1"
        "SomeCookie" "beamparticle@127.0.0.1" 1
        Java node launched with name=java@127.0.0.1
        Mailbox javaserver is listening...
        java.lang.ClassNotFoundException: JavaClassName
                at java.net.URLClassLoader.findClass(URLClassLoader.java:381)
                at java.lang.ClassLoader.loadClass(ClassLoader.java:424)
                at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:335)
                at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
                at java.lang.Class.forName0(Native Method)
                at java.lang.Class.forName(Class.java:264)
                at
        com.beamparticle.JavaLambdaNode.<init>(JavaLambdaNode.java:73)
                at com.beamparticle.Application.main(Application.java:14)
        Mailbox javaserver is listening...

--- support/javanode/src/main/java/com/beamparticle/Application.java
+++ support/javanode/src/main/java/com/beamparticle/Application.java
@@ -8,8 +8,9 @@ public class Application {
         // args[0] = name of the node
         // args[1] = cookie for node
         // args[2] = name of the parent erlang node
+        // args[3] = number of workers
         System.out.println("Java node launched with name=" + args[0]);
-        JavaLambdaNode lambdaNode = new JavaLambdaNode();
-        lambdaNode.start(args[0], args[1], args[2]);
+        JavaLambdaNode lambdaNode = new JavaLambdaNode(
+			args[0], args[1], args[2], Integer.parseInt(args[3]));
     }
 }
