e6cea5e65582c73ee9c66f73219b5d6bcea5a506
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2018-01-07 11:43:14 +0530

    Compile and load python source once forever
    It is much faster to compile given python source and load it
    once forever, but then every subsequent load will keep
    updating that, while adding more and more functions and
    classes. Additionally, all the classes and functions are
    in same namespace, so they can potentially call each other.
    It is important to note that the name given "a2" while
    calling __dynamic__ is just a placeholder to save the
    source code within the python node. It can serve an
    additional purpose of allowing "a2" to be directly
    callable from the Erlang node, since it knows about it.
    
    There is a key difference while invoking dynamic functions
    in python; namely the arguments are passed as tuple rather
    than list (because list can be confused with string in python
    node). Hence the choice of using {M, F, A},
    where M = <<"__dynamic__">>, F :: binary() and A :: {arg1, arg2, ...}
    
        (beamparticle@127.0.0.1)17> gen_server:call({'pythonserver',
        'python-1-beamparticle@127.0.0.1'}, {<<"MyProcess">>, <<"load">>, {<<"a2">>, <<"def
        add(a,b): return a+b\ndef a2(a,b): return add(a,b)">>}}).
        ok
        (beamparticle@127.0.0.1)18> gen_server:call({'pythonserver',
        'python-1-beamparticle@127.0.0.1'}, {<<"__dynamic__">>, <<"a2">>, {1,2}}).
        3

--- support/pynode/pynode.py
+++ support/pynode/pynode.py
@@ -17,6 +17,24 @@ class MyProcess(Process):
     def __init__(self, node) -> None:
         Process.__init__(self, node)
         node.register_name(self, term.Atom('pythonserver'))
+        self.dynamic_functions = {}
+        self.code_globals = {}
+        self.code_locals = {}
+
+    def load(self, fnamebin, codebin):
+        try:
+            dynamic_function_name = fnamebin.bytes_.decode('utf-8')
+            code = codebin.bytes_.decode('utf-8')
+            c = compile(code, '<string>', 'exec')
+            exec(c, self.code_globals, self.code_locals)
+            # copy locals to globals because otherwise
+            # eval('..', globals, locals) will fail since
+            # the depending functions will not be available in globals
+            self.code_globals.update(self.code_locals)
+            self.dynamic_functions[dynamic_function_name] = code
+            return (term.Atom('ok'))
+        except Exception as E:
+            return (term.Atom('error'), str(E))
 
     def handle_inbox(self):
         while True:
@@ -38,14 +56,69 @@ class MyProcess(Process):
         # message_ fields
 
         if True:
-            # Send a reply
-            gencall.reply(local_pid=self.pid_,
-                          result="ok")
+            # gencall is of type gen.GenIncomingMessage
+            if type(gencall.message_) != tuple or len(gencall.message_) != 3:
+                error_message = term.Binary(
+                        "Only {M :: binary(), F :: binary(), Args :: tuple()} messages allowed".encode('utf-8'))
+                gencall.reply(local_pid=self.pid_,
+                              result=(term.Atom('error'), error_message))
+            else:
+                (module, function, arguments) = gencall.message_
+                module_name = module.bytes_.decode('utf-8')
+                function_name = function.bytes_.decode('utf-8')
+                if module_name == 'MyProcess' and function_name == 'load':
+                    result = self.load(*arguments)
+                elif module_name == '__dynamic__':
+                    try:
+                        # Arguments must be a tuple because using List
+                        # can result into Erlang([1,2]) -> '\x01\x02'
+                        # which is not expected
+                        if not isinstance(arguments, tuple):
+                            raise Exception('Arguments must be a tuple but is ' + str(type(arguments)))
+                        #fun_ref = self.code_locals[function_name]
+                        #result = fun_ref(*arguments)
+                        # Instead lets run within the context, so that
+                        # local functions and imports are available during
+                        # the execution
+                        call_arguments = []
+                        for i in range(0, len(arguments)):
+                            argname = 'arg' + str(i) + '__'
+                            # save arguments to code_locals
+                            self.code_locals[argname] = arguments[i]
+                            call_arguments.append(argname)
+                        call_command = \
+                                function_name + '(' \
+                                + ','.join(call_arguments) \
+                                + ')'
+                        #print('call_command=' + call_command)
+                        #print('code_locals=' + str(self.code_locals))
+                        #print('code_globals=' + str(self.code_globals))
+                        result = eval(call_command,
+                                self.code_globals,
+                                self.code_locals)
+                        # cleanup code_locals to save memory
+                        for i in range(0, len(arguments)):
+                            argname = 'arg' + str(i) + '__'
+                            del self.code_locals[argname]
+                    except Exception as E:
+                        result = (term.Atom('error'), term.Binary(str(E).encode('utf-8')))
+                else:
+                    # DONT allow generic calls directly, instead
+                    # let user define dynamic functions and then within
+                    # that call those functions if required
 
+                    #fun_ref = eval(module_name + '.' + function_name,
+                    #        self.code_globals,
+                    #        self.code_locals)
+                    #result = fun_ref(*arguments)
+                    result = (term.Atom('error'), term.Binary('only dynamic calls allowed'.encode('utf-8')))
+                # Send a reply
+                gencall.reply(local_pid=self.pid_,
+                             result=result)
         else:
             # Send an error exception which will crash Erlang caller
             gencall.reply_exit(local_pid=self.pid_,
-                               reason="error")
+                               reason=term.Atom('error'))
 
 
 def main():
@@ -63,6 +136,7 @@ def main():
     #    # Sleep gives other greenlets time to run
     #    gevent.sleep(0.1)
 
+    # Lets run single mailbox at present
     gen_server = MyProcess(node)
     gen_server.handle_inbox()
     
