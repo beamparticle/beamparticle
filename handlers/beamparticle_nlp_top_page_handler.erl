8e37dd273c324c808ad16d5e517d169996b05dc8
Neeraj Sharma <neeraj.sharma@alumni.iitg.ernet.in>
2018-02-22 14:26:34 +0530

    use stage version with env=2 for dynamic html as well

--- handlers/beamparticle_nlp_top_page_handler.erl
+++ handlers/beamparticle_nlp_top_page_handler.erl
@@ -31,12 +31,20 @@
 -export([init/2]).
 
 init(Req0, Opts) ->
+    #{env := Env} = cowboy_req:match_qs([{env, [], <<"1">>}], Req0),
+    case Env of
+        <<"2">> ->
+            erlang:put(?CALL_ENV_KEY, stage);
+        _ ->
+            erlang:put(?CALL_ENV_KEY, prod)
+    end,
     case beamparticle_dynamic:execute({<<"nlpfn_top_page">>, [Req0, Opts]}) of
         {ok, {Body, RespHeaders}} when (is_list(Body) orelse
                                         is_binary(Body)) andalso
                                        is_map(RespHeaders) ->
             Req = cowboy_req:reply(200, RespHeaders, Body, Req0),
             lager:info("Req = ~p, Body = ~p", [Req, Body]),
+            erlang:erase(?CALL_ENV_KEY),
             {ok, Req, Opts};
 		_ ->
 			PrivDir = code:priv_dir(?APPLICATION_NAME),
@@ -44,5 +52,6 @@ init(Req0, Opts) ->
 			Size = filelib:file_size(File),
             lager:info("static file = ~p, size = ~p", [File, Size]),
 			Req = cowboy_req:reply(200, #{}, {sendfile, 0, Size, File}, Req0),
+            erlang:erase(?CALL_ENV_KEY),
             {ok, Req, Opts}
 	end.
